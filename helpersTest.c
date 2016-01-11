6f0248e707f802c31204c8a4a9dfefe99bd50a7d
David Kinol <dkinol@umich.edu>
2016-01-10 21:28:42 -0500

    add helpers.c

---
+++ helpersTest.c
@@ -0,0 +1,15 @@
+//
+//  h.c
+//  GitTest
+//
+//  Created by David Kinol on 1/10/16.
+//  Copyright © 2016 David Kinol. All rights reserved.
+//
+
+#include "helpersTest.h"
+
+
+
+void printInfo(){
+    printf("print info function\n");
+}
